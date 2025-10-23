bf3acd988a5f32a6c309b3d9f7aa00815d299a12
CKWG <christ-usch.grein@t-online.de>
2025-10-23 23:01:25 +0200

    Add files via upload

--- Test/test_si_vectors.adb
+++ Test/test_si_vectors.adb
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------------
 -- Checked and Unchecked Computation with SI Units
--- Copyright (C) 2020, 2025 Christoph Karl Walter Grein
+-- Copyright (C) 2025 Christoph Karl Walter Grein
 --
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License
@@ -27,59 +27,56 @@
 --   christ-usch.grein@t-online.de
 ------------------------------------------------------------------------------
 
-with Test_Rational_Arithmetics,
-     Test_Rational_Image_Value,
-     Test_Rational_Text_IO;
-with Test_SI_Units;
-with Test_SI_Math,
-     Test_SI_Poly;
-with Test_SI_Vectors;
-with Test_SI_Celsius;
-with Test_SI_Unformatted_IO;
-with Test_SI_Text_IO_Strings,
-     Test_SI_Text_IO_File,
-     Test_SI_Text_IO_Put;
+with Elementary_Functions;
+use  Elementary_Functions;
+
+with SI.VS;
+use  SI.VS, SI;
 
 with Test_Support;
 use  Test_Support;
 
-procedure Run_All is
+procedure Test_SI_Vectors is
 
   --====================================================================
   -- Author    Christoph Grein
-  -- Version   2.1
+  -- Version   1.0
   -- Date      23 October 2025
   --====================================================================
-  -- Run all tests and display the global result.
+  -- Test D = V * T, D and V vectors.
+  -- More tests in directory Examples.
   --====================================================================
   -- History
   -- Author Version   Date    Reason for change
-  --  C.G.    1.0  14.05.2020
-  --  C.G.    1.1  01.07.2025 new Test_SI_Celsius
-  --  C.G.    2.0  16.10.2025 'Image redefined
-  --  C.G.    2.1  23.10.2025 Test_SI_Vectors new
+  --  C.G.    1.0  23.10.2025
   --====================================================================
+
+  N: constant Vector := Normalize ((1.0, 1.0, 1.0));
+
+  D: constant Position :=  N * (1.0*"km");
+  V: constant Velocity := -N * (1.0*"dam/min");
 
 begin
 
-  Test_Rational_Arithmetics;
-  Test_Rational_Image_Value;
-  Test_Rational_Text_IO;
+  Test_Header (Title       => "Test Vectorspace",
+               Description => "Linear motion with constant velocity.");
 
-  Test_SI_Units;
+  -----------------------------------------------------------------------
+  Test_Step (Title       => "Test vector movement",
+             Description => "Compute time to reach home.");
 
-  Test_SI_Math;
-  Test_SI_Poly;
-  Test_SI_Vectors;
+  Put_Line (Position'(D + V * (abs D / abs V))'Image);  -- less accurate than scalar
+  Put_Line ("Distance to origin:" & Length'(abs (D + V * (abs D / abs V) - 1.0*"µm" * Null_Vector))'Image);
+  Assert (Condition => abs (D + V * (abs D / abs V) - 1.0*"µm" * Null_Vector) <= 0.11*"mm",
+          Message   => "(Vector) Time to reach origin:" & Time'(abs D / abs V)'Image,
+          Only_Report_Error => False);
 
-  Test_SI_Celsius;
+  Put_Line (Length'(abs D - abs V * (abs D / abs V))'Image);
+  Assert (Condition => abs D - abs V * (abs D / abs V) = 0.0*"dam",
+          Message   => "(Salar) Time to reach origin:" & Time'(abs D / abs V)'Image,
+          Only_Report_Error => False);
 
-  Test_SI_Unformatted_IO;
+  -----------------------------------------------------------------------
+  Test_Result;
 
-  Test_SI_Text_IO_Strings;
-  Test_SI_Text_IO_File;
-  Test_SI_Text_IO_Put;
-
-  Global_Result;
-
-end Run_All;
+end Test_SI_Vectors;
